ca76868dcdc4707cd4588ecbfb47d40836f42468
Emmanuel Adeleye <x00152620@mytudublin.ie>
2022-04-21 12:22:48 +0100

    server side done

---
+++ CA2_Server/Books.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CA2_Server
+{
+    public class Books
+    {
+        [ForeignKey("BookID")]
+        [Required]
+        [Key]
+        public int BookID { get; set; }
+        public string BookName { get; set; }
+
+        public string Description { get; set; }
+
+        public int price { get; set; }
+
+    }
+
+    public class Author 
+    {
+        [Required]
+        [Key]
+        public int AuthorID { get; set; }
+        public string AuthorName { get; set; }
+        public int Age { get; set; }
+        public DateTime DOB { get; set; }
+
+        public int BookID { get; set; }
+
+    }
+}
---
+++ CA2_Server/Data/CA2_ServerContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using CA2_Server;
+
+namespace CA2_Server.Data
+{
+    public class CA2_ServerContext : DbContext
+    {
+        public CA2_ServerContext (DbContextOptions<CA2_ServerContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<CA2_Server.Books> Books { get; set; }
+
+        public DbSet<CA2_Server.Author> Author { get; set; }
+    }
+}
